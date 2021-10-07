0da42a1ea06b83c337b5ebb6f33eb186c10e7c70
Jeimy Natalia <jeimynataliaolarte@gmail.com>
2021-10-06 21:50:40 -0500

    Se agrega la funcion para cargar dinamicamente las compras

--- javaScript/compras.js
+++ javaScript/compras.js
@@ -16,6 +16,12 @@ listaProducto = [producto1, producto2, producto3, producto4, producto5]
 let nombresEnvio= ["Express", "Normal"];
 let valoresEnvio= [20000, 10000];
 
+let listaCompras=[
+    {cliente:"jose", documento:"1021938102", producto:producto5, cantidad:20,
+     envio:1, descuento:100000, total:910000
+    },
+];
+
 const cargarProductos=()=>{
 
     let select=document.getElementById("productos");
@@ -23,23 +29,45 @@ const cargarProductos=()=>{
     for (let index = 0; index < listaProducto.length; index++) {
         const producto = listaProducto[index];
         select.innerHTML+=`<option value="${index}">
-        ${producto.nombre} - $${producto.precio}</option>`;
+        ${producto.nombre} - $${producto.precio}
+        </option>`;
     }
 };
 
 cargarProductos();
 
 const agregarCompra = (compra)=>{
+
     let tabla = document.getElementById("datos_compras");
+
     tabla.innerHTML += `<tr>
-    <td>${compra.cliente} (${compra.documento})</td>
-    <td>${compra.producto.nombre}-$${compra.producto.precio}</td>
-    <td>${compra.cantidad}</td>
-    <td>${nombresEnvio[compra.envio]}</td>
-    <td>$${compra.descuento}</td>
-    <td>$${compra.total}</td>
-</tr>`;
-;}
+                <td>${compra.cliente} (${compra.documento})</td>
+                <td>${compra.producto.nombre} - $${compra.producto.precio}</td>
+                <td>${compra.cantidad}</td>
+                <td>${nombresEnvio[compra.envio]}</td>
+                <td>$${compra.descuento}</td>
+                <td>$${compra.total}</td>
+            </tr>`;
+
+};
+
+const cargarCompras =()=>{
+    let tabla = document.getElementById("datos_compras");
+    tabla.innerHTML="";
+
+    for (const unaCompra of listaCompras) {
+     tabla.innerHTML+= `<tr>
+     <td>${unaCompra.cliente} (${unaCompra.documento})</td>
+     <td>${unaCompra.producto.nombre}-$${unaCompra.producto.precio}</td>
+     <td>${unaCompra.cantidad}</td>
+     <td>${nombresEnvio[unaCompra.envio]}</td>
+     <td>$${unaCompra.descuento}</td>
+     <td>$${unaCompra.total}</td>
+ </tr>`;  
+    }
+}
+
+cargarCompras();
 
 const procesa_info=()=>{
     
@@ -49,34 +77,40 @@ const procesa_info=()=>{
     compra.cliente = formulario.cliente.value;
     compra.documento = formulario.documento.value;
     let seleccion = formulario.producto.value; 
-    compra.producto=listaProducto[seleccion-1];
+    compra.producto=listaProducto[seleccion];
     compra.cantidad = formulario.cantidad.value;
     compra.envio = formulario.envio.value;
     
     compra.total=0;
     compra.descuento=0;
 
-    let subtotal=compra.producto.precio*compra.cantidad;
+    let subtotal = compra.producto.precio*compra.cantidad;
     let valorEnvio=0;
 
     if(compra.cantidad>10){
         compra.descuento = subtotal*0.1;
     }
 
+
     if(subtotal>5000000){
         if (compra.envio==0) {
-            valorEnvio=valoresEnvio[1];    
+            valorEnvio = valoresEnvio[1];
+            
         } else {
             valorEnvio=0;
         }
-        compra.descuento+=valoresEnvio[compra.envio]-valorEnvio;
+        compra.descuento += valoresEnvio[compra.envio] - valorEnvio;
     }else{
-        valorEnvio=valoresEnvio[compra.envio];
+        valorEnvio = valoresEnvio[compra.envio];
     }
 
-    compra.total = valorEnvio+subtotal-compra.descuento;
+
+    compra.total = valorEnvio + subtotal - compra.descuento;
+
     formulario.reset();
-    agregarCompra(compra);
+    listaCompras.push(compra);
+    cargarCompras();
+    // agregarCompra(compra);
 };
 
 
